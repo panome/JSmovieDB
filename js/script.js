c69c761f9c17f79fc917ab169bf2048c6ad6926a
Ilya <Omeljan@post.cz>
2020-08-06 17:55:25 +0200

    Changed background

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 /* Задания на урок:
 
-1) Удалить все рекламные блоки со страницы (правая часть сайта)
+1) Удалить все рекламные блоки со страницы (правая часть сайта)+
 
-2) Изменить жанр фильма, поменять "комедия" на "драма"
+2) Изменить жанр фильма, поменять "комедия" на "драма"+
 
 3) Изменить задний фон постера с фильмом на изображение "bg.jpg". Оно лежит в папке img.
 Реализовать только при помощи JS
@@ -26,6 +26,7 @@ const movieDB = {
 
 const   adv = document.querySelector('.promo__adv'),
         advImg = adv.querySelectorAll('img'),
+        background = document.querySelector('.promo__bg'),
         genre = document.querySelector('.promo__genre')
 
 const deleteAdv = (selector) => {
@@ -39,3 +40,8 @@ const changeGenre = (selector) => {
     selector.innerHTML = 'drama'
 }
 changeGenre(genre)
+
+const changeBackground = (selector) => {
+    selector.style.backgroundImage = 'url(./img/bg.jpg)'
+}
+changeBackground(background)
