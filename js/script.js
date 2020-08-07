976bb4667869bd8e82c0d973e501f49990abf37f
Ilya <Omeljan@post.cz>
2020-08-07 15:30:38 +0200

    Complete add movie from form and add to favorite

--- js/script.js
+++ js/script.js
@@ -1,16 +1,19 @@
 /* Задания на урок:
 
-1) Удалить все рекламные блоки со страницы (правая часть сайта)+
+1) Реализовать функционал, что после заполнения формы и нажатия кнопки "Подтвердить" - 
+новый фильм добавляется в список. Страница не должна перезагружаться.
+Новый фильм должен добавляться в movieDB.movies.
+Для получения доступа к значению input - обращаемся к нему как input.value;
+P.S. Здесь есть несколько вариантов решения задачи, принимается любой, но рабочий.
 
-2) Изменить жанр фильма, поменять "комедия" на "драма"+
+2) Если название фильма больше, чем 21 символ - обрезать его и добавить три точки
 
-3) Изменить задний фон постера с фильмом на изображение "bg.jpg". Оно лежит в папке img.+
-Реализовать только при помощи JS
+3) При клике на мусорную корзину - элемент будет удаляться из списка (сложно)
 
-4) Список фильмов на странице сформировать на основании данных из этого JS файла.+
-Отсортировать их по алфавиту 
+4) Если в форме стоит галочка "Сделать любимым" - в консоль вывести сообщение: 
+"Добавляем любимый фильм"
 
-5) Добавить нумерацию выведенных фильмов */
+5) Фильмы должны быть отсортированы по алфавиту */
 
 'use strict';
 
@@ -28,7 +31,11 @@ const   adv = document.querySelector('.promo__adv'),
         advImg = adv.querySelectorAll('img'),
         background = document.querySelector('.promo__bg'),
         genre = document.querySelector('.promo__genre'),
-        movieList = document.querySelector('.promo__interactive-list')
+        movieList = document.querySelector('.promo__interactive-list'),
+        form = document.querySelector('.add'),
+        formInput = document.querySelector('.adding__input'),
+        checkbox = document.querySelector('input[type=checkbox]'),
+        btn = document.querySelector('button')
 
 const deleteAdv = (selector) => {
     selector.forEach(item => {
@@ -58,3 +65,18 @@ const addMovieFromDB = (selector) => {
     })
 }
 addMovieFromDB(movieList)
+
+const addMovieFromForm = () => {
+    btn.addEventListener('click', (e) => {
+        e.preventDefault()
+
+        movieDB.movies.push(formInput.value)
+        if(checkbox.checked) {
+            console.log(`Pridal jste ${formInput.value} do oblibeneho!`)
+        }
+        movieList.innerHTML = ''
+        addMovieFromDB(movieList)
+        form.reset()
+    })
+}
+addMovieFromForm()
